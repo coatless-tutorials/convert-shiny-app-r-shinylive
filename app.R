e6ed926855ceab008348523b0fb4ea4c7b2ae24c
James J Balamuta <james.balamuta@gmail.com>
2024-03-22 00:19:50 -0700

    Add the Shiny app: app.R

---
+++ app.R
@@ -0,0 +1,50 @@
+# Based on SO Question:
+# https://stackoverflow.com/questions/78153629/static-version-of-r-shiny-app-using-shinylive-package-failing-to-preload-package
+
+# Load required libraries
+library(shiny)
+library(shinythemes)
+
+# Define UI
+ui <- fluidPage(
+  theme = shinytheme("flatly"),
+  
+  # App title
+  titlePanel("CSV File Uploader"),
+  
+  # Sidebar layout with input and output definitions
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Choose CSV File", accept = ".csv"),
+      tags$hr(),
+      checkboxInput("header", "Header", TRUE),
+      checkboxInput("stringAsFactors", "Convert strings to factors", TRUE)
+    ),
+    
+    # Show CSV data
+    mainPanel(
+      tableOutput("contents")
+    )
+  )
+)
+
+# Define server logic
+server <- function(input, output) {
+  
+  # Read CSV file
+  data <- reactive({
+    req(input$file)
+    df <- read.csv(input$file$datapath,
+                   header = input$header,
+                   stringsAsFactors = input$stringAsFactors)
+    return(df)
+  })
+  
+  # Show CSV data
+  output$contents <- renderTable({
+    data()
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
